272158c38b46743706ccbc1b839d1d9bacd3c971
Manuel Alejandro Garisto Zuna <garistozunamanuel@gmail.com>
2021-10-18 11:24:24 -0400

    flexbox-wrapping

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="box box1">1</div>
+      <div className="box box2">2</div>
+      <div className="box box3">3</div>
+      <div className="box box4">4</div>
+      <div className="box box5">5</div>
+      <div className="box box6">6</div>
+      <div className="box box7">7</div>
+      <div className="box box8">8</div>
+      <div className="box box9">9</div>
+      <div className="box box10">10</div>
+      
     </div>
   );
 }
